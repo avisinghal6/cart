2cd3a3d2cbaf2daa112c2d294ff14d61623dcf46
avi singhal <97785770+avisinghal6@users.noreply.github.com>
2022-04-15 00:10:49 +0530

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import CartItem from './cartItem';
 import Cart from './Cart';
+import Navbar from './Navbar';
+
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <h1> cart </h1>
       <Cart />
 
