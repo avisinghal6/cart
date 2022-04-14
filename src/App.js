effee24cb40810446705439d7bdefed86b64fad1
avi singhal <97785770+avisinghal6@users.noreply.github.com>
2022-04-14 14:55:55 +0530

    create cart component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import CartItem from './cartItem';
-
+import Cart from './Cart';
 
 function App() {
   return (
     <div className="App">
       <h1> cart </h1>
-      <CartItem />
+      <Cart />
 
       
     </div>
--- src/cartItem.js
+++ src/cartItem.js
@@ -10,7 +10,31 @@ class CartItem extends React.Component{
             qty:1,
             img: ''
         }
+
+        
     }
+
+    //testing
+    //setState function can only be used when the component has rendered. Because it s a function of the React Component claas
+    // and is available only after rendering, thats why we cannot use this.testing inside the constructor
+    // to observe the synchronous behaviour of setState inside promises. Inside promises, setState does not do batching.
+    //BUT THIS MIGHT HAVE BEEN FIXED IN THE LATEST VERSION AND THATS WHY IT MIGHT ALWAYS BE ASYNCHRONOUS AND BATCHING MIGHT BE PERFORMED EVERYWHERE.
+
+    // testing =() =>{
+    //     const promise = new Promise((resolve,reject) => {
+    //         setTimeout(() =>{
+    //             resolve('done');
+    //         }, 5000);
+    //     })
+
+    //     promise.then(() => {
+    //         //setState acts like a synchronous call
+
+    //         this.setState({qty: 100});
+
+    //         console.log('state', this.state);
+    //     });
+    // }
 
     increaseQuantity= () => { //used arrow functions as they inherit the scope of 'this'
         // console.log('this', this.state);
@@ -27,12 +51,30 @@ class CartItem extends React.Component{
         // });
         }
 
+    decreaseQuantity= () => { //used arrow functions as they inherit the scope of 'this'
+        const {qty} = this.state;
+        if(qty ===0)
+            return;
+        this.setState({
+            qty:this.state.qty -1
+        });
+    
+            //setState form 2, pass a callback
+            // this.setState((prevState) => {
+            //     return {
+            //         qty: prevState.qty -1
+            //     }
+            // });
+    }
+
         //Using setState function, React is actually doing shallow merging, which means it will only change the property
         // specified by us in the setState function, in this case 'qty', other properties will not be touched.
         // after changing the 'qty', 'render' function is called by react to rerender the page.
 
-    render(){
+     render(){
         const { price, title, qty} = this.state; //object destructuring
+        console.log("render");
+        
         return (
             
             <div className="cart-item">
@@ -57,7 +99,7 @@ class CartItem extends React.Component{
                             alt="decrease" 
                             className="action-icons" 
                             src="https://cdn-icons-png.flaticon.com/512/992/992683.png" 
-                            
+                            onClick= {this.decreaseQuantity}
                             />
                         <img 
                             alt="delete" 
