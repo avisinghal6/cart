ef985db99a4e7650b84707fb8f7578f9d566806d
avi singhal <97785770+avisinghal6@users.noreply.github.com>
2022-04-16 01:13:13 +0530

    added firebase config

--- src/index.js
+++ src/index.js
@@ -2,9 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import * as serviceWorker from './serviceWorker';
+// import * as firebase from 'firebase';
+// import 'firebase/firestore';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const firebaseConfig = {
+//add firebase config here
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+
 root.render(
   <React.StrictMode>
     <App />
