b348c2fb28a8e0b0c59f5eb3fef5e07bab0717fd
avi singhal <97785770+avisinghal6@users.noreply.github.com>
2022-04-16 01:25:47 +0530

    added data from firebase

--- src/App.js
+++ src/App.js
@@ -2,38 +2,42 @@ import React from 'react';
 import CartItem from './cartItem';
 import Cart from './Cart';
 import Navbar from './Navbar';
-
+import firebase from 'firebase/compat/app';
 
 class App extends React.Component {
   constructor(){
     super();
     this.state={
-        products: [ 
-            {
-                price: 99,
-                title: 'watch',
-                qty: 1,
-                img: 'https://cdn-icons.flaticon.com/png/512/3109/premium/3109881.png?token=exp=1649963946~hmac=2e71e38a3e3d72f04dd6c6b53c0ed290',
-                id: 1
-            },
-            {
-                price: 909,
-                title: 'mobile phone',
-                qty: 1,
-                img: 'https://cdn-icons.flaticon.com/png/512/2586/premium/2586488.png?token=exp=1649963971~hmac=042810909c2a163044c72cbde02eb3f8',
-                id: 2
-            },
-            {
-                price: 999,
-                title: 'laptop',
-                qty: 1,
-                img: 'https://cdn-icons-png.flaticon.com/512/413/413837.png',
-                id: 3
-            },
-        ]
+        products: [],
+        loading :true
     }
 
     
+}
+
+componentDidMount () {
+  firebase
+    .firestore()
+    .collection('products')
+    .get()
+    .then((snapshot) => {
+      // console.log(snapshot);
+      snapshot.docs.map((doc) => { //docs is an array
+      //  console.log(doc.data);
+
+      });
+      
+      const products = snapshot.docs.map((doc) => {
+        const data= doc.data();
+        data['id']= doc.id;
+        return data;
+      });
+
+      this.setState({
+        products,
+        loading: false
+      })
+    })
 }
 
 handleIncreaseQuantity= (product) => { 
@@ -94,7 +98,7 @@ getCartTotal= ()=> {
 
  
   render () {
-    const {products} = this.state;
+    const {products, loading} = this.state;
       return (
         <div className="App">
           <Navbar count={this.getCartCount()} />
@@ -105,7 +109,7 @@ getCartTotal= ()=> {
           onDecreaseQuantity={this.handleDecreaseQuantity}
           onDelete= {this.handleDeleteProduct}
           />
-
+        {loading && <h1> loading products ..</h1>}
           <div style={ {padding:10, fontSize:20}}> TOTAL: {this.getCartTotal()} </div>
         </div>
         );
